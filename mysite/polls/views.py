3bdc3a88bcb49e1c3fbf1a81a8e08fe23135b55c
Daniel Teixeira Silva <eugostodequeijadinhadequeijo+github@gmail.com>
2017-05-01 01:32:55 -0300

    Configuração dos estáticos
    Etapa 6 do tutorial.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
